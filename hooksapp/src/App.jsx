0075006d745921345a6e2c9e653e9974075aa7b7
Julen37 <julen.gouchault@gmail.com>
2025-09-25 11:13:44 +0200

    maj

--- hooksapp/src/App.jsx
+++ hooksapp/src/App.jsx
@@ -25,76 +25,76 @@ function App() {
     {name: "Lisa", age: 8}
   )
 
-  // const [countOne, setCountOne] = useState({value: 0, btnColor: 'success', increment: 25 })
-  // const [countTwo, setCountTwo] = useState({value: 0, btnColor: 'danger', increment: 20 })
+  const [countOne, setCountOne] = useState({value: 0, btnColor: 'success', increment: 25 })
+  const [countTwo, setCountTwo] = useState({value: 0, btnColor: 'danger', increment: 20 })
 
-  // const  incrementCountOne = useCallback((val) => {
-  //   console.log('je suis dans incrementcountone')
-  //   countOne.value < 100 && setCountOne({...countOne, value: countOne.value + val})
-  // }, [countOne])
+  const  incrementCountOne = useCallback((val) => {
+    console.log('je suis dans incrementcountone')
+    countOne.value < 100 && setCountOne({...countOne, value: countOne.value + val})
+  }, [countOne])
   // declaration de la fonction fléchée qui prend pour parametre val qui sera la valeur ajouté
   // si la valeur de countone est strictement inferieur a 100 on execute la suite
   // on prend les données du state de countone et on modifie son ancienne valeur en ajoutant la nouvelle (aka l'increment 25 ici qu'on rappel dans le bouton)
   // le usecallback sert ici a ce que ca refresh seulement le countone et pas le counttwo avec quand countone est changé
 
-  // const  incrementCountTwo = useCallback((val) => {
-  //   countTwo.value < 100 && setCountTwo({...countTwo, value: countTwo.value + val})
-  // }, [countTwo])
+  const  incrementCountTwo = useCallback((val) => {
+    countTwo.value < 100 && setCountTwo({...countTwo, value: countTwo.value + val})
+  }, [countTwo])
 
 //router
-  const [isAdmin, setIsAdmin] = useState(true); // ou false pour non-admin, 
+  // const [isAdmin, setIsAdmin] = useState(true); // ou false pour non-admin, 
   // si on met false on pourra plus aller sur profileRouter car ca dirigera sur home
 
   return (
-    <BrowserRouter>
-        <nav>
-          <Link to="/">Home</Link> | <Link to="/about">About</Link> | <Link to="/profile">Profile</Link>
-        </nav>
-        <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/about" element={<About/>}/>
-          <Route path="/profile" 
-            element={
-              <PrivateRoute isAdmin={isAdmin}>
-                <ProfileRouter user={user} />
-              </PrivateRoute>
-            }
-          />
-        </Routes>
-    </BrowserRouter>
+    // <BrowserRouter>
+    //     <nav>
+    //       <Link to="/">Home</Link> | <Link to="/about">About</Link> | <Link to="/profile">Profile</Link>
+    //     </nav>
+    //     <Routes>
+    //       <Route path="/" element={<Home/>}/>
+    //       <Route path="/about" element={<About/>}/>
+    //       <Route path="/profile" 
+    //         element={
+    //           <PrivateRoute isAdmin={isAdmin}>
+    //             <ProfileRouter user={user} />
+    //           </PrivateRoute>
+    //         }
+    //       />
+    //     </Routes>
+    // </BrowserRouter>
 
-      // <div className='container'>
-      //   <h1>useState Hooks</h1>
-      //   <ClassState/>
-      //   <hr />
-      //   <FunctionState/>
-      //   <hr />
-      //   <Todo/>
-      //   <hr />
-      //   <p>Class Count</p>
-      //  <ClassCount/>
-      //   <hr />
-      //   <p>Function Count</p>
-      //   <FunctionCount/>
-      //   <hr />
-      //   <TestUseEffect/>
-      //   <hr />
-      //   <p>exo 10 / UseContext</p>
-      //   <UserContext.Provider value={user}>
-      //     <ColorContext.Provider value={'pink'}>
-      //       <Profile/>
-      //     </ColorContext.Provider>
-      //   </UserContext.Provider>
-      //   <hr />
-      //   <p>exo 11 / UseReducer</p>
-      //   <CountReducer/>
-      //   <hr />
-      //   <p>UseCallBack</p>
-      //   <CountBar text="CountOne" count={countOne.value} bgColor={countOne.btnColor}/>
-      //   <CountBar text="CountTwo" count={countTwo.value} bgColor={countTwo.btnColor}/>
-      //   <Button handleClick={incrementCountOne} btnColor={countOne.btnColor} increment={countOne.increment}> Count 1</Button>
-      //   <Button handleClick={incrementCountTwo} btnColor={countTwo.btnColor} increment={countTwo.increment}> Count 2</Button>
-      // </div>
+      <div className='container'>
+        <h1>useState Hooks</h1>
+        <ClassState/>
+        <hr />
+        <FunctionState/>
+        <hr />
+        <Todo/>
+        <hr />
+        <p>Class Count</p>
+       {/* <ClassCount/> */}
+        <hr />
+        <p>Function Count</p>
+        <FunctionCount/>
+        <hr />
+        <TestUseEffect/>
+        <hr />
+        <p>exo 10 / UseContext</p>
+        <UserContext.Provider value={user}>
+          <ColorContext.Provider value={'pink'}>
+            <Profile/>
+          </ColorContext.Provider>
+        </UserContext.Provider>
+        <hr />
+        <p>exo 11 / UseReducer</p>
+        <CountReducer/>
+        <hr />
+        <p>UseCallBack</p>
+        <CountBar text="CountOne" count={countOne.value} bgColor={countOne.btnColor}/>
+        <CountBar text="CountTwo" count={countTwo.value} bgColor={countTwo.btnColor}/>
+        <Button handleClick={incrementCountOne} btnColor={countOne.btnColor} increment={countOne.increment}> Count 1</Button>
+        <Button handleClick={incrementCountTwo} btnColor={countTwo.btnColor} increment={countTwo.increment}> Count 2</Button>
+      </div>
   )
 }
 
